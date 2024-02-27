835d278de76f59350bd8a5b1c0fc9a960de9ed1c
shaikhaakif03 <akif64176@gmail.com>
2024-02-27 11:58:46 +0530

    done

--- project/index.php
+++ project/index.php
@@ -7,8 +7,7 @@
     <title>Oslo Colour Festival - Holi in Norway</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/akc-sheep.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -20,13 +19,14 @@
     <!-- main section start  -->
     <section class="akc-holi">
         <div class="container">
-            <div class="row">
-            </div>
         </div>
     </section>
     <!-- main section end -->
     <!-- day counter  -->
     <section>
+        <div class="row akc-timer text-center py-2 W-100">
+            <div id="timer"></div>
+        </div>
     </section>
     <!-- day counter end  -->
     <!-- galary section start  -->
@@ -208,9 +208,8 @@
     include 'elements/footer.php';
     ?>
     <!--footer section end -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
