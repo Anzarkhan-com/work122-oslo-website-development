e4bbe2eec70f237fafb7b88d1f4fc197abbc63c7
shaikhaakif03 <akif64176@gmail.com>
2024-03-09 17:08:42 +0530

    done

--- project/elements/header.php
+++ project/elements/header.php
@@ -9,27 +9,33 @@
                 <a class="nav-link active" href="#"><button type="button" class="btn btn-custom2 btn-sm ">LIVE <i class="fa-solid fa-circle-play" style="color: #ffffff;"></i> </button></a>
             </div>
             <div class="collapse navbar-collapse align-items-baseline" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
+                <ul class="navbar-nav ms-auto align-items-center">
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="#">ABOUT US </a>
+                        <a class="nav-link active fs-15 " href="#">ABOUT US </a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="event-details.php">EVENT DETAILS</a>
+                        <a class="nav-link active fs-15" href="event-details.php">EVENT DETAILS</a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="#">SPONSORSHIP</a>
+                        <a class="nav-link active fs-15" href="#">SPONSORSHIP</a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="infopack.php">INFOPACK</a>
+                        <a class="nav-link active fs-15" href="infopack.php">INFOPACK</a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="gallery.php">GALLERY</a>
+                        <a class="nav-link active fs-15" href="gallery.php">GALLERY</a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="#">FAQ'S</a>
+                        <a class="nav-link active fs-15" href="#">FAQ'S</a>
                     </li>
+                    <div class="akc-home-divider"></div>
                     <li class="nav-item akc-fw-large ">
-                        <a class="nav-link active" href="#">CONTACT</a>
+                        <a class="nav-link active fs-15" href="#">CONTACT</a>
                     </li>
                 </ul>
                 <div class="d-flex flex-column ml">
@@ -40,7 +46,7 @@
                         <a class="nav-link active" href="#"><i class="fa-brands fa-tiktok"></i></a>
                     </div>
                     <div class="pt-2">
-                        <a class="nav-link active" href="#"><button type="button" class="btn btn-custom"><i class="fa-solid fa-share" style="color: #ffffff;"></i> Book Now</button></a>
+                        <a class="nav-link active" href="#"><button type="button" class="btn btn-custom btn-sm"><i class="fa-solid fa-share" style="color: #ffffff;"></i> Book Now</button></a>
                     </div>
                 </div>
             </div>
