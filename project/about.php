15604b1aa36cb597a96f543c8c7a313f7a4bf1b5
najifa <najifagena@gmail.com>
2025-04-04 18:15:26 +0530

    about-us

--- project/about.php
+++ project/about.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/css/new-style.css">
     <link rel="stylesheet" href="assets/css/akc-sheep.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 
@@ -57,35 +58,61 @@
     <!-- Our Blog Area Start -->
     <div class="our-blog-area section-padding-100">
         <div class="container">
+      
+        <h3>About Us</h3>
+        <p class="new-ed-text">Since its debut in 2022, the Oslo Colour Festival has become a vibrant highlight in Oslo’s cultural scene. The festival began with 2,500 attendees in its first year and quickly grew, welcoming 4,000 visitors in 2023 and 5,000 in 2024. From the start, the festival has offered a rich tapestry of cultural performances, delicious cuisine, and the joyous tradition of playing with colors. The festival has showcased over 500 artists, featuring diverse dance styles and musical acts from around the world, alongside a lively street food market.  </p>
 
-            <h3>Who are we?</h3>
+            <h3>Bringing the Colours to the Heart of Oslo  </h3>
             <p class="new-ed-text">
-                In 2024, the Oslo Colour Festival emerged as a vibrant highlight in Oslo's cultural landscape. On May 6th 2023, Vallhall Arena hosted over 4,000 attendees who enjoyed a rich tapestry of cultural performances, delightful cuisine, and the joyful tradition of playing with colors. Our event featured more than 500 artists showcasing diverse dance styles and musical acts from around the globe, complemented by a lively street food festival.
+            As we look ahead to 2025, the festival is set to be bigger and more spectacular than ever! This year, we’re bringing the celebration to the heart of Oslo, making it more accessible to a wider audience. Expect an explosion of talent, mouthwatering Indian cuisine, and the exhilarating experience of playing with colors alongside friends and family.  
 
             </p>
+
+            <p class="new-ed-text">The Oslo Colour Festival is more than just an event—it’s a celebration of life, culture, and diversity. With even more dazzling performances and cultural showcases planned, we invite you to be part of an unforgettable experience.  </p>
 
             <!-- <p style="color:black">The Oslo Colour Festival is organized by IndoNord - The Association of Indo-Norwegian Diaspora in Norway. IndoNord derives its uniqueness and identity from the fact that it is the apex organization for promoting the economic, cultural, and social welfare of the Indian diaspora in Norway. We are eager to share and showcase the cultural gifts of India along with our friends in Norway. The contributions of our range of partners, who share our enthusiasm, play an important role in our efforts to spread our love, unity, diversity, and togetherness.</p> -->
             <br>
-            <h3>Origin of the Oslo Colour Festival</h3>
-            <p class="new-ed-text">As we look forward to 2025, the festival is poised to become even bigger and grander. We are thrilled to welcome renowned performers from Norway and abroad, including top-tier artists, musicians, and dancers. This year, we are especially excited to host prominent artists traveling all the way from India, bringing with them a vibrant spectrum of talents that promise to enrich our celebration. Additionally, we are embracing new-age technology to enhance the immersive experience of our color celebrations, ensuring an unforgettable event for all attendees. </p>
-            <p class="new-ed-text">The Oslo Colour Festival aims to be a cornerstone event in Oslo’s annual cultural calendar, a celebration of life and diversity. With more spectacular performances and cultural showcases planned, we invite you to join us for what promises to be an unforgettable event.
-
-
-            </p>
+            <h3>Oslo Colour Festival 2025  </h3>
+            <p class="new-ed-text"><i class="fa-regular fa-floppy-disk"></i>  Save the date: June 7th, 2025  </p>
+            <p class="new-ed-text"><i class="fa-solid fa-location-dot"></i>  Location: Spikersuppa, Karl Johans Gate, Oslo  </p>
+            <p class="new-ed-text"><i class="fa-solid fa-check"></i>  Free Entry!</p>
 
             <!-- <p style="color:black">The Oslo Colour Festival is a celebration of diversity and cultural display, where colors are used with the intention of spreading the hues of life. The festival brings the rich culture of India, with the idea of unification, harmony, and celebration. The festival also presents fresh vibes and experiences to its attendees and takes them on a journey of rediscovery.</p> -->
             <!-- <p style="color:black">The festival is a fusion of various cultural elements, including people, cultures, food, art, music, ideas, and thoughts. Our inaugural event in 2022 attracted around 3,000 attendees and included stellar group performances by over 300 participants.</p> -->
             <br>
-            <h3>Oslo Color Festival 2025</h3>
+           
             <p class="new-ed-text">
-                Save the date for June 7th, 2025! Join us for a breathtaking celebration of diversity and life in colors.
-                Stay tuned for more details!</p>
-                <p class="new-ed-text">Venue: Spikersuppa Karl Johans Gate.
+            Join us for a breathtaking celebration of diversity, music, dance, and vibrant colors! Stay tuned for more details.
+            </p>
+            <h3>Hvem er vi?  </h3>
+            <p class="new-ed-text">
+            Siden oppstarten i 2022 har Oslo Colour Festival blitt et fargerikt høydepunkt i Oslos kulturliv. Festivalen startet med 2500 deltakere det første året og vokste raskt, med 4000 besøkende i 2023 og 5000 i 2024. Helt fra begynnelsen har festivalen bydd på et rikt mangfold av kulturelle opptredener, deilig mat og den gøyale tradisjonen med å leke med farger. Festivalen har presentert over 500 artister, med alt fra ulike dansestiler til musikalske innslag fra hele verden, i tillegg til en livlig street food-festival.  </p>
+               
+            <br>
+
+            <h3>Vi tar med fargene til hjertet av Oslo  </h3>
+            <p class="new-ed-text">Når vi nå ser frem mot 2025, er festivalen klar til å bli større og mer spektakulær enn noensinne! I år flytter vi feiringen til hjertet av Oslo, slik at enda flere kan ta del i opplevelsen. Gjør deg klar for et fyrverkeri av talent, smakfull indisk mat og den fantastiske følelsen av å leke med farger sammen med venner og familie.</p>
 
             <!-- <p style="color:black"> Oslo Colour Festival aspires to become one of the landmark events in Oslo’s annual cultural calendar, celebrating life and diversity. On May 6th, 2023, we are expecting an attendance of over 4,000 people who will come and experience a variety of cultural performances, delicious cuisine, and the unique experience of playing with colors.</p> -->
             <!-- <p style="color:black">We will have over 500+ artists performing in various dance formats of Indian and global origin, a street food festival, dance and music, and celebrating diversity through the use of colors.</p>
             <p style="color:black">We look forward to seeing you on May 6th, 2023, at Vallhall Arena, Dronning Margretes Vei 11, 0663 Oslo to experience this unique celebration.</p> -->
+       <p class="new-ed-text"> 
+       Oslo Colour Festival er mer enn bare et arrangement – det er en feiring av liv, kultur og mangfold. Med enda flere spektakulære opptredener og kulturelle innslag på programmet, inviterer vi deg til å bli med på en uforglemmelig opplevelse!  
+       </p>
+
+       <br>
+       <h3>Oslo Colour Festival 2025  </h3>
+            <p class="new-ed-text"><i class="fa-regular fa-floppy-disk"></i> Hold av datoen: 7. juni 2025  </p>
+            <p class="new-ed-text"><i class="fa-solid fa-location-dot"></i>  Sted: Spikersuppa, Karl Johans gate, Oslo  
+            </p>
+            <p class="new-ed-text"><i class="fa-solid fa-check"></i>  Gratis inngang!  
+            </p>
+       
+       <br>
+
+       <p class="new-ed-text">Bli med på en fantastisk feiring av mangfold, musikk, dans og livlige farger! Følg med for flere detaljer.</p>
         </div>
+
     </div>
     </div>
     <!-- Our Blog Area End -->
--- project/infopack.php
+++ project/infopack.php
@@ -51,34 +51,39 @@
     </section>
     <!-- Breadcrumb Area End -->
     <!-- Our Blog Area Start -->
-    <div class="our-blog-area section-padding-100" style="padding-top:25px !important;">
+    <div class="our-blog-area section-padding-100">
         <div class="container">
+
             <ul>
                 <li>Here you will find important and practical information about the <b>Oslo Colour Festival.</b> </li>
-                <li>Please read the document carefully and if you have any queries, please do not hesitate to contact us at
-                    <a href="mailto:OCF@indonord.org">OCF@indonord.org</a>
+                <li>Please read the document carefully and if you have any queries, please do not hesitate to contact us at 
+                    <a href="mailto:info@oslocolourfestival.no">info@oslocolourfestival.no</a>
                 </li>
             </ul>
             </br>
+
             <h3>Event Details</h3>
             <ul>
                 <table class="table">
+
                     <tbody>
                         <tr>
                             <td>Date</td>
-                            <td>Saturday 4th May 2024 </td>
+                            <td>Saturday 7th June 2025 10:00 to 22:00</td>
                         </tr>
                         <tr>
                             <td>Arrival Time</td>
-                            <td>Registration and Check-in starts at 10:00 AM .</td>
+                            <td>Registration and Check-in starts at 10:00 hrs.
+                            </td>
                         </tr>
                         <tr>
                             <td>Venue</td>
-                            <td>Vallhall Arena, Dronning Margretes vei 11, 0663 Oslo</td>
+                            <td>Spikersuppa, karl Johans Gate
+                            </td>
                         </tr>
                         <tr>
                             <td>Contact Details</td>
-                            <td><a href="mailto:OCF@indonord.org">OCF@indonord.org
+                            <td><a href="mailto:info@oslocolourfestival.no">info@oslocolourfestival.no
                                 </a></td>
                         </tr>
                         <!-- <tr>
@@ -86,17 +91,20 @@
                             <td>
                                 <div class="container">
                                     <div class="row align-items-center">
-                                        <div class="col-12 col-md-6">
+                                         <div class="col-12 col-md-6">
                                             <div class="about-thumb mb-80 wow fadeInUp" data-wow-delay="300ms">
                                                 <h3 class="wow fadeInUp" data-wow-delay="300ms">Scan with smartphone camera</h3>
                                                 <img src="assets/img/core-img/activities.png" style="width: 200px; height: 200px;" alt="">
                                             </div>
                                         </div> 
+                                        
                                         <div class="col-12 col-md-6">
                                             <div class="about-content-text mb-80">
                                                 <a href="/activities.html" class="btn confer-btn w-100 mb-30">Click Here<i class="zmdi zmdi-long-arrow-right"></i></a>
                                             </div>
                                         </div>
+                                        
+
                                     </div>
                                 </div>
                             </td>
@@ -108,7 +116,7 @@
             </br>
             <h5>Practical Information</h5></br>
             <ul>
-                <li style="list-style: decimal;list-style-position: inside;">Check-in starts at 10:00 AM.</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Event  starts at 10:00.</span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Keep QR code/EntryPass visible for smooth check-in. </span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Entry-pass (QR Code) can be scanned only once.</span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Parking at Vallhall is only allowed for pre-booked cars. </span></li>
@@ -118,27 +126,36 @@
             </br></br>
             <h5>Food</h5></br>
             <ul>
-                <li style="list-style: decimal;list-style-position: inside;">Food from outside is not allowed, it can be purchased at Food counters.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">If you have any food allergies, check ingredients before buying food.</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Food from outside is not allowed, it can be purchased at Food counters.
+                </span></li>
+                <li style="list-style: decimal;list-style-position: inside;">If you have any food allergies, check ingredients before buying food.
+                </span></li>
             </ul>
             </br></br>
             <h5>Color Celebration</h5></br>
             <ul>
-                <li style="list-style: decimal;list-style-position: inside;">Colour festival will be celebrated outside the arena.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">No outside Colours will be allowed for playing. Colour will be provided.</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Color festival would only be played in a designated area.
+                </span></li>
+                <li style="list-style: decimal;list-style-position: inside;">No outside Colours will be allowed for playing Color play. Colors to be purchased from the designated area </span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Do not use chemical infused or harmful colours</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">Don't apply colours in the area near to eyes, nose, mouth and open wounds.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">Use of water during colour festival is not allowed</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">Kids should be accompanied by parents.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">It is recommended to have an extra pair of clothes to participate in colour festival.</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Don't apply colours in the area near to eyes, nose, mouth and open wounds.
+                </span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Use of water during color celebration is not allowed</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Kids should be accompanied by parents.
+                </span></li>
+                <li style="list-style: decimal;list-style-position: inside;">It is recommended to have an extra pair of clothes to participate in color festival.
+                </span></li>
             </ul>
             </br></br>
             <h5>General</h5></br>
             <ul>
-                <li style="list-style: decimal;list-style-position: inside;">Keep the wristband on your wrist throughout the event.</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Follow instructions provided by Volunteers during the event.
+                </span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Follow instructions provided by Volunteers during the event.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">Parents take full responsibility for their children throughout the event at the venue.</span></li>
-                <li style="list-style: decimal;list-style-position: inside;">It is advised to wear shoes all the time (Valhall arena is an Indoor football stadium with an artificial grass surface).</span></li>
+                <li style="list-style: decimal;list-style-position: inside;">Parents take full responsibility for their children throughout the event at the venue.
+                </span></li>
+                <li style="list-style: decimal;list-style-position: inside;">It is advised to wear shoes all the time 
+                </span></li>
                 <li style="list-style: decimal;list-style-position: inside;">Consumption of intoxicating substances such as tobacco, alcohol, etc is strictly prohibited in and around the venue.</span></li>
                 <li style="list-style: decimal;list-style-position: inside;">We reserve the right to decide and remove any participant deemed physically incapable to be at the event thus preventing him/her from causing greater harm and injury to himself/herself or others.</span></li>
                 <li style="list-style: decimal;list-style-position: inside;">For terms and conditions please check: <a href="terms.php"><i class="zmdi zmdi-circle"></i> Terms of use</a></span></li>
